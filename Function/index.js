939b5e0f2029ae1f0e6c1ec3fa810a9e6c8865c5
Nhan Vi Nam <nhanvinamcoder@gmail.com>
2020-09-02 18:11:15 +0700

    Finish lession 5

---
+++ Function/index.js
@@ -0,0 +1,11 @@
+// Viết hàm tính diện tích tam giác
+const dienTichTamGiac = (a, h) => {
+  return (a * h) / 2;
+};
+console.log(dienTichTamGiac(10, 5));
+
+// Viết hàm tính diện tích khối hộp
+const dienTichKhoiHop = (w, h, l) => {
+  return w * h * l;
+};
+console.log(dienTichKhoiHop(10, 20, 30));
