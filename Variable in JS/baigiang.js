9e622334ba073f85e03cc45fcc76f81db5673ad6
Nhan Vi Nam <nhanvinamcoder@gmail.com>
2020-09-02 16:21:12 +0700

    Finish lession 1

---
+++ Variable in JS/baigiang.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
+
+// Khi chạy chương trình này, máy tính sẽ chạy từ trên xuống dưới
+// Khi chạy, biến a và b sẽ được khai báo và gán giá trị 1 và 2 vào RAM, CPU sẽ thực hiện tính toán a + b, và máy tính sẽ khai báo biến c và gán giá trị đã
+// tính toán vào biến c trong RAM, cuối cùng sẽ xuất ra màn hình biến c.
+// Kết thúc chương trình, các biến trong RAM sẽ biến mất
